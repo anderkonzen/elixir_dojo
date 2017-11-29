7cf4686d32e1c59469ff035090a5802ae2c1bdc2
Anderson Konzen <anderson.konzen@gmail.com>
2017-11-29 01:34:02 -0200

    Exercicio realizado em 28/Nov/2017, na Meetup Elixir POA.

--- lib/elixir_dojo.ex
+++ lib/elixir_dojo.ex
@@ -1,18 +1,14 @@
 defmodule ElixirDojo do
-  @moduledoc """
-  Documentation for ElixirDojo.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirDojo.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def init(size \\ 100) do
+    Enum.map(1..size, &fizz_buzz/1)
   end
+
+  def fizz_buzz(number) when rem(number, 15) == 0,
+    do: "fizzbuzz"
+  def fizz_buzz(number) when rem(number, 3) == 0,
+    do: "fizz"
+  def fizz_buzz(number) when rem(number, 5) == 0,
+    do: "buzz"
+  def fizz_buzz(number), do: number
+
 end
--- test/elixir_dojo_test.exs
+++ test/elixir_dojo_test.exs
@@ -1,8 +1,50 @@
 defmodule ElixirDojoTest do
   use ExUnit.Case
-  doctest ElixirDojo
 
-  test "greets the world" do
-    assert ElixirDojo.hello() == :world
+  test "deve iniciar com uma lista de 100 elementos" do
+    assert ElixirDojo.init() |> Enum.count() == 100
+  end
+
+  test "deve iniciar com uma lista de N elementos" do
+    assert ElixirDojo.init(50) |> Enum.count() == 50
+    assert ElixirDojo.init(60) |> Enum.count() == 60
+  end
+
+  test "com 3 elementos deve retornar 1,2,fizz" do
+    assert ElixirDojo.init(3) == [1, 2, "fizz"]
+  end
+
+  test "com 5 elementos deve retornar 1,2,fizz, 4, buzz" do
+    assert ElixirDojo.init(5) == [1, 2, "fizz", 4, "buzz"]
+  end
+
+  test "com 15 elementos, o 15º elemento deve ser fizzbuzz" do
+    assert Enum.at(ElixirDojo.init(15), 14) == "fizzbuzz"
+  end
+
+  test "deve retornar 2 fizz e 1 buzz" do
+    result =
+      90
+      |> ElixirDojo.init()
+      |> Enum.slice(82, 6)
+
+    assert result == [83, "fizz", "buzz", 86, "fizz", 88]
+  end
+
+  test "numero divisivel por 3 deve imprimir fizz" do
+    assert ElixirDojo.fizz_buzz(3) == "fizz"
+  end
+
+  test "numero divisivel por 5 deve imprimir fizz" do
+    assert ElixirDojo.fizz_buzz(5) == "buzz"
+  end
+
+  test "numero divisivel por 3 e 5 deve imprimir fizzbuzz" do
+    assert ElixirDojo.fizz_buzz(15) == "fizzbuzz"
+    assert ElixirDojo.fizz_buzz(30) == "fizzbuzz"
+  end
+
+  test "qualquer outro número deve retornar o próprio número" do
+    assert ElixirDojo.fizz_buzz(124) == 124
   end
 end
